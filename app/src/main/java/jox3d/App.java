07bb9c1d85b6136ab70e56bf6232a7598d2f95b0
JackiBackiBoy <TheGameTail@gmail.com>
2022-01-17 12:24:49 +0100

    Cleanup

--- app/src/main/java/jox3d/App.java
+++
@@ -1,9 +0,0 @@
-package jox3d;
-import jox3d.sandbox.*;
-
-public class App {
-  public static void main(String[] args) {
-    RenderingWindow window = new RenderingWindow();
-      window.run();
-  }
-}
